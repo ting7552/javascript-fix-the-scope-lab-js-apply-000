5e523ba66700e721096c73dee05d47e4a5882191
Shelly T <ixchell.tingling@gmail.com>
2018-03-20 19:19:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var animal = 'dog'
 
 function myAnimal() {
@@ -28,4 +29,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()
+var theFunk = funkyFunction
